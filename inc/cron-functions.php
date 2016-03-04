b9fd2cf5c49a52fe1986757c37551603083de3e4
tntsoft <tnt_soft@ymail.com>
2016-03-04 08:25:20 +0200

    Latest Cron

--- inc/cron-functions.php
+++ inc/cron-functions.php
@@ -3,6 +3,8 @@ function autoApproveIndividualAward($id)
 {
 	global $db;
 
+	$award = getNomination($id);
+
 	//first we set the approval status as 1 in the table
 	$rs = $db->prepare("UPDATE tblnominations WHERE id LIKE :id SET AprStatus = '1', AprDate = NOW()");
 	$rs->execute(array(":id" => $id));
@@ -16,108 +18,147 @@ function autoApproveIndividualAward($id)
 		$stmt->execute();
 	}
 
-	//let's get all details we need for this nomination
+	$awardOptions = '&pound;20 voucher';
+
+	//now let's get all work awards from the database
+	$workawards = $db->prepare("SELECT * FROM tblworkawards");
+	$workawards->execute();
+	while($workaward = $workawards->fetch(PDO::FETCH_OBJ))
+	{
+		$awardOptions .= ",".$workaward->type;
+	}
+
+	//remove last ; from the string
+	$awardOptions = substr($awardOptions, 0, -1);
+
 	$nomination = $db->prepare(
-		"SELECT n.BeliefID as BeliefID,
-				n.personalMessage as personalMessage,
-				LM.Eaddress as LMEaddress, 
-				LM.Fname as LMFname, 
-				CONCAT(approver.Fname, ' ', approver.Sname) as approverFullname,
-				approver.Eaddress as approverEaddress, 
-				approver.Fname as approverFname,
-				nominator.Eaddress as nominatorEaddress,
-				nominator.Fname as nominatorFname, 
-				nominator.Sname as nominatorSname, 
-				CONCAT(nominator.Fname, ' ', nominator.Sname) as nominatorFullname, 
-				CONCAT(nominee.Fname, ' ', nominee.Sname) as nomineeFullname,
-				nominee.Offline as nomineeOffline,
-				nominee.Fname as nomineeFname,
-				nominee.Sname as nomineeSname, 
-				FROM tblnominations as n 
-				JOIN tblempall as nominator ON nominator.EmpNum = n.NominatorEmpNum 
-				JOIN tblempall as nominee ON nominee.EmpNum = n.NominatedEmpNum 
-				JOIN tblempall as approver ON approver.EmpNum = n.ApproverEmpNum 
-				JOIN tblempall as LM ON lm.EmpNum = nominee.LMEmpNum WHERE n.id LIKE :id");
+	"SELECT CONCAT(approver.Fname, ' ', approver.Sname) as approverFullname,
+			approver.Eaddress as approverEaddress, 
+			approver.Fname as approverFname,
+			FROM tblnominations as n 
+			JOIN tblempall as approver ON approver.EmpNum = n.ApproverEmpNum 
+			WHERE n.id LIKE :id");
+	$nomination->execute(array(":id" => $id));
 	$nomination = $nomination->fetch(PDO::FETCH_OBJ);
 
-	//now let's start sending out some emails
+	//Let's hook the approver up with his stuff
+	$approver_name = $nomination->approverFullname;
+	$approver_fname = $nomination->approverFname;
+	$approver_email = $nomination->approverEaddress;
 
 	//----------------------------------
-	// Shoot an email to the Nominator
+	// Shoot an email to the nominator
 	//----------------------------------
-	$sendEmail = new StdClass();
-	$sendEmail->emailTo = $nomination->nominatorEaddress;
-	$sendEmail->subject = 'Congratulations, your nomination has been approved';
-	$sendEmail->Content = "<p>Dear ".$nomination->nominatorFname."</p>
-							<p>Good news!</p>
-							<p>Your nomination of ".$nomination->nomineeFullname." for an Our Heroes Extraordinary People, Extraordinary Effort Award has been approved by ".$nomination->approverFullname.".</p>
-							<p>The Thank You certificate you prepared when nominating this award has been sent by email to ".$nomination->nomineeFullname." with details of the award.</p>
-							<p>A record of all the nominations you have made and their current status can be viewed on the <a href='".HTTP_PATH."'>My Awards</a> section of the Our Heroes portal.</p>
-							<p>Thank you for your nomination.</p>
-							<p>Kind Regards</p>";
-	$email = sendEmail($sendEmail,$id);
-
+	if($award->ApproverEmpNum != $award->NominatorEmpNum)
+	{
+		if(filter_var($award->nominator()->Eaddress, FILTER_VALIDATE_EMAIL))
+		{
+			$sendEmail = new StdClass();
+			$sendEmail->emailTo = $award->nominator()->Eaddress;
+			$sendEmail->subject = 'Congratulations, your nomination has been approved';
+			$sendEmail->Content = "<p>Dear ".$award->nominator()->Fname."</p>
+									<p>Good news!</p>
+									<p>Your nomination of ".$award->nominee()->full_name." for an Our Heroes Extraordinary People, Extraordinary Effort Award has been approved by ".$approver_name.".</p>
+									<p>The Thank You certificate you prepared when nominating this award has been sent by email to ".$award->nominee()->full_name." with details of the award.</p>
+									<p>A record of all the nominations you have made and their current status can be viewed on the <a href='".HTTP_PATH."'>My Awards</a> section of the Our Heroes portal.</p>
+									<p>Thank you for your nomination.</p>
+									<p>Kind Regards</p>";
+			$email = sendEmail($sendEmail,$id);
+		} 
+		else 
+		{
+			$email = "fail";
+		}
+	} 
+		
 	//----------------------------------
-	// Shoot an email to the line manager
+	// Shoot an email to the Line Manager
 	//----------------------------------
-	$sendEmail = new StdClass();
-	$sendEmail->emailTo = $nomination->LMEaddress;
-	$sendEmail->subject = "Congratulations, someone in your team has been sent a 'Little Extra' award";
-	$sendEmail->Content = "<p>Dear ".$nomination->LMFname."</p>
-							<p>Good news!</p>
-							<p>".$nomination->nomineeFullname." in your team has been given an Our Heroes Extraordinary People, Extraordinary Effort award by ".$nomination->nominatorFullname." for the ".$nomination->BeliefID." category.</p>
-							<p>".$nomination->nomineeFname." was also given 'A Little Extra' as part of the award and can choose from. This was approved by ".$nomination->approverFullname.".</p>
-							<p>Please congratulate ".$nomination->nomineeFname." in person and to consider other members of your team who may be suitable for a nomination on the <a href='".HTTP_PATH."'>Our Heroes</a> portal.</p>
-							<p>Kind Regards</p>";
-
-	$email = sendEmail($sendEmail,$id);
+	if($award->ApproverEmpNum != $award->lineManager()->EmpNum)
+	{
+		if(filter_var($award->lineManager()->Eaddress, FILTER_VALIDATE_EMAIL))
+		{
+			$sendEmail = new StdClass();
+			$sendEmail->emailTo = $award->lineManager()->Eaddress;
+			$sendEmail->subject = "Congratulations, someone in your team has been sent a 'Little Extra' award";
+			$sendEmail->Content = "<p>Dear ".$award->lineManager()->Fname."</p>
+									<p>Good news!</p>
+									<p>".$award->nominee()->full_name." in your team has been given an Our Heroes Extraordinary People, Extraordinary Effort award by ".$award->nominator()->full_name." for the ".$award->BeliefID." category.</p>
+									<p>".$award->nominee()->Fname." was also given 'A Little Extra' as part of the award and can choose from: ".$awardOptions.". This was approved by ".$approver_name.".</p>
+									<p>Please congratulate ".$award->nominee()->Fname." in person and to consider other members of your team who may be suitable for a nomination on the <a href='".HTTP_PATH."'>Our Heroes</a> portal.</p>
+									<p>Kind Regards</p>";
+									print_r($sendEmail);
+			$email = sendEmail($sendEmail,$id);
+			echo $sendEmail->Content;
+		} 
+		else 
+		{
+			$email = "fail";
+		}
+	} 	
 
 	//----------------------------------
 	// Shoot an email to the approver
 	//----------------------------------
-	$sendEmail = new StdClass();
-	$sendEmail->emailTo = $nomination->approverEaddress;
-	$sendEmail->subject = "Confirmation of your approval for a 'Little Extra' award";
-	$sendEmail->Content = "<p>Dear ".$nomination->approverFname."</p>
-							<p>Thank you for approving 'A Little Extra' award for ".$nomination->nomineeFullname.". The details of this award are as follows:<p>
-							<p>Nominator: ".$award->nominator()->full_name."<br>
-							Award Options: ".$awardOptions."
-							<br>Award category: ".$award->BeliefID."</p>
-							<p>Any other nominations awaiting your approval can be found in the <a href='".HTTP_PATH."'>My Approvals</a> section of the Our Heroes Portal.</p>
-							<p>Thank you</p>";
-	$email = sendEmail($sendEmail,$id);
-
+	if(filter_var($approver_email, FILTER_VALIDATE_EMAIL))
+	{
+		$sendEmail = new StdClass();
+		$sendEmail->emailTo = $approver_email;
+		$sendEmail->subject = "Confirmation of your approval for a 'Little Extra' award";
+		$sendEmail->Content = "<p>Dear ".$approver_fname."</p>
+								<p>Thank you for approving 'A Little Extra' award for ".$award->nominee()->full_name.". The details of this award are as follows:<p>
+								<p>Nominator: ".$award->nominator()->full_name."<br>
+								Award Options: ".$awardOptions."
+								<br>Award category: ".$award->BeliefID."</p>
+								<p>Any other nominations awaiting your approval can be found in the <a href='".HTTP_PATH."'>My Approvals</a> section of the Our Heroes Portal.</p>
+								<p>Thank you</p>";
+		$email = sendEmail($sendEmail,$id);
+	}
+	
 	//----------------------------------
 	// Shoot an email to the nominee
 	//----------------------------------
+	$award->Fname = $award->nominee()->Fname;
+	if ($award->Volunteer !='') 
+	{
+		$award->NomFull_name = $award->Volunteer;
+	} 
+	else 
+	{
+		$award->NomFull_name = $award->nominator()->full_name;
+	}
 
-	//First let's construct the ECard
-	$ecard = new StdClass();
-	$ecard->Fname = $nomination->nomineeFname;
-	$ecard->NomFull_name = $nomination->nominatorFullname;
-	$ecard->BeliefID = $nomination->BeliefID;
-	$ecard->personalMessage = $nomination->personalMessage;
-	$ecard->NomFname = $nomination->nominatorFname;
-
-	$content = indEcardExtraText($ecard);
-
-	//If the nominee if offline, shoot an email to the administrator of xexec
-	if($nomination->nomineeOffline == 'Y')
+	$award->NomFname = $award->nominator()->Fname;
+	$award->content = indEcardExtraText($award);
+	echo $award->content;
+	
+	// If the nominee is offline, send an email to the xexec admin
+	// else send him an email
+	if ($award->nominee()->Offline == 'Y')
 	{
 		$sendEmail = new StdClass();
 		$sendEmail->emailTo = $xexecEmail;
 		$sendEmail->Cc = 'hillarypress@btconnect.com';
 		$sendEmail->subject = 'E-Card Award Notification';
 		$sendEmail->Content = "<p>Hi</p>
-								<p>".$nomination->nominatorFullname." has nominated ".$nomination->nomineeFullname." to receive a Thank you card as part of an Our Heroes Award.</p>
+								<p>".$award->nominator()->full_name." has nominated ".$award->nominee()->full_name." to receive a Thank you card as part of an Our Heroes Award.</p>
 								<p>Below is the content of the card:</p>
-								<p>".$content."</p>";
+								<p>".$award->content."</p>";
 		$email = sendEmail($sendEmail,$id);
-	}
-	else //else just shoot him an email
+		echo "offline email sent to xxexec";
+	} 
+	else 
 	{
-		$ecard->subject = "Congratulations, you've been sent an Our Heroes award with 'A Little Extra'";
-		$email = sendEcardEmail($ecard,$id);
+		if(filter_var($award->nominee()->Eaddress, FILTER_VALIDATE_EMAIL))
+		{
+			$award->subject = "Congratulations, you've been sent an Our Heroes award with 'A Little Extra'";
+			$email = sendEcardEmail($award,$id);
+		} 
+		else 
+		{
+			$email = "fail";
+		}
+		echo $email;
 	}
 }
 
@@ -352,3 +393,113 @@ function constructCCSU()
 
 	return $cc;
 }
+
+/*
+function individualAwardsNotUsed($id)
+{
+	//let's get all details we need for this nomination
+	$nomination = $db->prepare(
+		"SELECT n.BeliefID as BeliefID,
+				n.personalMessage as personalMessage,
+				LM.Eaddress as LMEaddress, 
+				LM.Fname as LMFname, 
+				CONCAT(approver.Fname, ' ', approver.Sname) as approverFullname,
+				approver.Eaddress as approverEaddress, 
+				approver.Fname as approverFname,
+				nominator.Eaddress as nominatorEaddress,
+				nominator.Fname as nominatorFname, 
+				nominator.Sname as nominatorSname, 
+				CONCAT(nominator.Fname, ' ', nominator.Sname) as nominatorFullname, 
+				CONCAT(nominee.Fname, ' ', nominee.Sname) as nomineeFullname,
+				nominee.Offline as nomineeOffline,
+				nominee.Fname as nomineeFname,
+				nominee.Sname as nomineeSname, 
+				FROM tblnominations as n 
+				JOIN tblempall as nominator ON nominator.EmpNum = n.NominatorEmpNum 
+				JOIN tblempall as nominee ON nominee.EmpNum = n.NominatedEmpNum 
+				JOIN tblempall as approver ON approver.EmpNum = n.ApproverEmpNum 
+				JOIN tblempall as LM ON lm.EmpNum = nominee.LMEmpNum WHERE n.id LIKE :id");
+	$nomination->execute(array(":id" => $id));
+	$nomination = $nomination->fetch(PDO::FETCH_OBJ);
+
+	//now let's start sending out some emails
+
+	//----------------------------------
+	// Shoot an email to the Nominator
+	//----------------------------------
+	$sendEmail = new StdClass();
+	$sendEmail->emailTo = $nomination->nominatorEaddress;
+	$sendEmail->subject = 'Congratulations, your nomination has been approved';
+	$sendEmail->Content = "<p>Dear ".$nomination->nominatorFname."</p>
+							<p>Good news!</p>
+							<p>Your nomination of ".$nomination->nomineeFullname." for an Our Heroes Extraordinary People, Extraordinary Effort Award has been approved by ".$nomination->approverFullname.".</p>
+							<p>The Thank You certificate you prepared when nominating this award has been sent by email to ".$nomination->nomineeFullname." with details of the award.</p>
+							<p>A record of all the nominations you have made and their current status can be viewed on the <a href='".HTTP_PATH."'>My Awards</a> section of the Our Heroes portal.</p>
+							<p>Thank you for your nomination.</p>
+							<p>Kind Regards</p>";
+	$email = sendEmail($sendEmail,$id);
+
+	//----------------------------------
+	// Shoot an email to the line manager
+	//----------------------------------
+	$sendEmail = new StdClass();
+	$sendEmail->emailTo = $nomination->LMEaddress;
+	$sendEmail->subject = "Congratulations, someone in your team has been sent a 'Little Extra' award";
+	$sendEmail->Content = "<p>Dear ".$nomination->LMFname."</p>
+							<p>Good news!</p>
+							<p>".$nomination->nomineeFullname." in your team has been given an Our Heroes Extraordinary People, Extraordinary Effort award by ".$nomination->nominatorFullname." for the ".$nomination->BeliefID." category.</p>
+							<p>".$nomination->nomineeFname." was also given 'A Little Extra' as part of the award and can choose from: ".$awardOptions.". This was approved by ".$nomination->approverFullname.".</p>
+							<p>Please congratulate ".$nomination->nomineeFname." in person and to consider other members of your team who may be suitable for a nomination on the <a href='".HTTP_PATH."'>Our Heroes</a> portal.</p>
+							<p>Kind Regards</p>";
+
+	$email = sendEmail($sendEmail,$id);
+
+	//----------------------------------
+	// Shoot an email to the approver
+	//----------------------------------
+	$sendEmail = new StdClass();
+	$sendEmail->emailTo = $nomination->approverEaddress;
+	$sendEmail->subject = "Confirmation of your approval for a 'Little Extra' award";
+	$sendEmail->Content = "<p>Dear ".$nomination->approverFname."</p>
+							<p>Thank you for approving 'A Little Extra' award for ".$nomination->nomineeFullname.". The details of this award are as follows:<p>
+							<p>Nominator: ".$award->nominator()->full_name."<br>
+							Award Options: ".$awardOptions."
+							<br>Award category: ".$award->BeliefID."</p>
+							<p>Any other nominations awaiting your approval can be found in the <a href='".HTTP_PATH."'>My Approvals</a> section of the Our Heroes Portal.</p>
+							<p>Thank you</p>";
+	$email = sendEmail($sendEmail,$id);
+
+	//----------------------------------
+	// Shoot an email to the nominee
+	//----------------------------------
+
+	//First let's construct the ECard
+	$ecard = new StdClass();
+	$ecard->Fname = $nomination->nomineeFname;
+	$ecard->NomFull_name = $nomination->nominatorFullname;
+	$ecard->BeliefID = $nomination->BeliefID;
+	$ecard->personalMessage = $nomination->personalMessage;
+	$ecard->NomFname = $nomination->nominatorFname;
+
+	$content = indEcardExtraText($ecard);
+
+	//If the nominee if offline, shoot an email to the administrator of xexec
+	if($nomination->nomineeOffline == 'Y')
+	{
+		$sendEmail = new StdClass();
+		$sendEmail->emailTo = $xexecEmail;
+		$sendEmail->Cc = 'hillarypress@btconnect.com';
+		$sendEmail->subject = 'E-Card Award Notification';
+		$sendEmail->Content = "<p>Hi</p>
+								<p>".$nomination->nominatorFullname." has nominated ".$nomination->nomineeFullname." to receive a Thank you card as part of an Our Heroes Award.</p>
+								<p>Below is the content of the card:</p>
+								<p>".$content."</p>";
+		$email = sendEmail($sendEmail,$id);
+	}
+	else //else just shoot him an email
+	{
+		$ecard->subject = "Congratulations, you've been sent an Our Heroes award with 'A Little Extra'";
+		$email = sendEcardEmail($ecard,$id);
+	}
+}
+*/
